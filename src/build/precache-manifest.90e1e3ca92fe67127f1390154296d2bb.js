6d0184376bd5048d11be0c87f8752d638e089c91
BrunoTorresF <bruno.torres.fabila@gmail.com>
2020-06-12 16:35:10 -0500

    finish my-blog miniapp

---
+++ src/build/precache-manifest.90e1e3ca92fe67127f1390154296d2bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59afbb65a378163cb511bbfee47037fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe12c2ee4ec0328d5336",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "f911909b4bac5819b422",
+    "url": "/static/js/2.750881d8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.750881d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe12c2ee4ec0328d5336",
+    "url": "/static/js/main.97e08269.chunk.js"
+  },
+  {
+    "revision": "36adbc7179ce49d2ac8a",
+    "url": "/static/js/runtime-main.79e3e27f.js"
+  }
+]);
